c7ee657058744684c40b335cf7472b0121ad98e9
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-03-01 16:16:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,10 +173,14 @@ do
 	fi
 					
 	#    nym_systemd_run
-		
-	id=56
-	thuong=$((i/100))
-	serid=$((id + thuong))
+
+	id=10
+	if [ $i -lt $id ];
+	then 
+		serid=56
+	else
+		serid=57
+	fi	
 	service_id=$(cat /etc/systemd/system/${nymmixnode}.service | grep id | cut -c ${serid}-)
 
 	## Check if user chose a valid node written in the systemd.service file
@@ -202,21 +206,14 @@ do
 
 	
     printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
-    printf "%b\n" "${WHITE}                     Make sure to also check the official docs ! "
+    printf "%b\n" "${WHITE}                             SPAM NYM MIXNODE IPV6 ! "
     printf "%b\n\n\n"
-    printf "%b\n" "${LGREEN}                            https://nymtech.net/docs/"
+    printf "%b\n" "${LGREEN}                        		   {nym}
     printf "%b\n\n\n"
     printf "%b\n" "${WHITE}                              Check the dashboard"
     printf "%b\n\n\n"
     printf "%b\n" "${LBLUE}                          https://testnet-explorer.nymtech.net/"
     printf "%b\n\n\n"
-    printf "%b\n" "${WHITE}                                       or"
-    printf "%b\n\n\n"
-    printf "%b\n" "${YELLOW}                           ./nym_install.sh --status"
-    printf "%b\n\n\n"
-    printf "%b\n" "${WHITE}                              to see how many packets"
-    printf "%b\n\n\n"
-    printf "%b\n" "${WHITE}                            You have ${YELLOW} mixed ${WHITE} so far ! "
-    printf "%b\n\n\n"
     printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
+    sleep 5	
 done
